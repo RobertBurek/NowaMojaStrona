aa4a8d9c455f9a97909589eb893c87814af7b55e
Robert Burek <robertburek@wp.pl>
2020-06-27 14:46:38 +0200

    Inny rozkład hronologi dyplomów od L do P

--- js/dyplomy.js
+++ js/dyplomy.js
@@ -50,7 +50,7 @@ document.querySelector("#k1w1Slide1").appendChild(imgk1w1Slide1);
 
 let imgk1w2Slide1 = document.createElement('img');
 imgk1w2Slide1.alt = ("dyplom");
-imgk1w2Slide1.src = dyplomyPion[10];
+imgk1w2Slide1.src = dyplomyPion[8];
 document.querySelector("#k1w2Slide1").appendChild(imgk1w2Slide1);
 
 let imgk2w1Slide1 = document.createElement('img');
@@ -60,62 +60,62 @@ document.querySelector("#k2w1Slide1").appendChild(imgk2w1Slide1);
 
 let imgk2w2Slide1 = document.createElement('img');
 imgk2w2Slide1.alt = ("dyplom");
-imgk2w2Slide1.src = dyplomyPoziom[22];
+imgk2w2Slide1.src = dyplomyPoziom[19];
 document.querySelector("#k2w2Slide1").appendChild(imgk2w2Slide1);
 
 let imgk2w3Slide1 = document.createElement('img');
 imgk2w3Slide1.alt = ("dyplom");
-imgk2w3Slide1.src = dyplomyPoziom[21];
+imgk2w3Slide1.src = dyplomyPoziom[15];
 document.querySelector("#k2w3Slide1").appendChild(imgk2w3Slide1);
 
 let imgk3w1Slide1 = document.createElement('img');
 imgk3w1Slide1.alt = ("dyplom");
-imgk3w1Slide1.src = dyplomyPoziom[20];
+imgk3w1Slide1.src = dyplomyPoziom[22];
 document.querySelector("#k3w1Slide1").appendChild(imgk3w1Slide1);
 
 let imgk3w2Slide1 = document.createElement('img');
 imgk3w2Slide1.alt = ("dyplom");
-imgk3w2Slide1.src = dyplomyPoziom[19];
+imgk3w2Slide1.src = dyplomyPoziom[18];
 document.querySelector("#k3w2Slide1").appendChild(imgk3w2Slide1);
 
 let imgk3w3Slide1 = document.createElement('img');
 imgk3w3Slide1.alt = ("dyplom");
-imgk3w3Slide1.src = dyplomyPoziom[18];
+imgk3w3Slide1.src = dyplomyPoziom[14];
 document.querySelector("#k3w3Slide1").appendChild(imgk3w3Slide1);
 
 let imgk4w1Slide1 = document.createElement('img');
 imgk4w1Slide1.alt = ("dyplom");
-imgk4w1Slide1.src = dyplomyPion[9];
+imgk4w1Slide1.src = dyplomyPion[10];
 document.querySelector("#k4w1Slide1").appendChild(imgk4w1Slide1);
 
 let imgk4w2Slide1 = document.createElement('img');
 imgk4w2Slide1.alt = ("dyplom");
-imgk4w2Slide1.src = dyplomyPion[8];
+imgk4w2Slide1.src = dyplomyPion[7];
 document.querySelector("#k4w2Slide1").appendChild(imgk4w2Slide1);
 
 let imgk5w1Slide1 = document.createElement('img');
 imgk5w1Slide1.alt = ("dyplom");
-imgk5w1Slide1.src = dyplomyPoziom[17];
+imgk5w1Slide1.src = dyplomyPoziom[21];
 document.querySelector("#k5w1Slide1").appendChild(imgk5w1Slide1);
 
 let imgk5w2Slide1 = document.createElement('img');
 imgk5w2Slide1.alt = ("dyplom");
-imgk5w2Slide1.src = dyplomyPoziom[16];
+imgk5w2Slide1.src = dyplomyPoziom[17];
 document.querySelector("#k5w2Slide1").appendChild(imgk5w2Slide1);
 
 let imgk5w3Slide1 = document.createElement('img');
 imgk5w3Slide1.alt = ("dyplom");
-imgk5w3Slide1.src = dyplomyPoziom[15];
+imgk5w3Slide1.src = dyplomyPoziom[13];
 document.querySelector("#k5w3Slide1").appendChild(imgk5w3Slide1);
 
 let imgk6w1Slide1 = document.createElement('img');
 imgk6w1Slide1.alt = ("dyplom");
-imgk6w1Slide1.src = dyplomyPoziom[14];
+imgk6w1Slide1.src = dyplomyPoziom[20];
 document.querySelector("#k6w1Slide1").appendChild(imgk6w1Slide1);
 
 let imgk6w2Slide1 = document.createElement('img');
 imgk6w2Slide1.alt = ("dyplom");
-imgk6w2Slide1.src = dyplomyPoziom[13];
+imgk6w2Slide1.src = dyplomyPoziom[16];
 document.querySelector("#k6w2Slide1").appendChild(imgk6w2Slide1);
 
 let imgk6w3Slide1 = document.createElement('img');
@@ -125,7 +125,7 @@ document.querySelector("#k6w3Slide1").appendChild(imgk6w3Slide1);
 
 let imgk7w1Slide1 = document.createElement('img');
 imgk7w1Slide1.alt = ("dyplom");
-imgk7w1Slide1.src = dyplomyPion[7];
+imgk7w1Slide1.src = dyplomyPion[9];
 document.querySelector("#k7w1Slide1").appendChild(imgk7w1Slide1);
 
 let imgk7w2Slide1 = document.createElement('img');
@@ -137,92 +137,92 @@ document.querySelector("#k7w2Slide1").appendChild(imgk7w2Slide1);
 // Slide 2
 let imgk1w1Slide2 = document.createElement('img');
 imgk1w1Slide2.alt = ("dyplom");
-imgk1w1Slide2.src = dyplomyPion[11];
+imgk1w1Slide2.src = dyplomyPion[5];
 document.querySelector("#k1w1Slide2").appendChild(imgk1w1Slide2);
 
 let imgk1w2Slide2 = document.createElement('img');
 imgk1w2Slide2.alt = ("dyplom");
-imgk1w2Slide2.src = dyplomyPion[10];
+imgk1w2Slide2.src = dyplomyPion[2];
 document.querySelector("#k1w2Slide2").appendChild(imgk1w2Slide2);
 
 let imgk2w1Slide2 = document.createElement('img');
 imgk2w1Slide2.alt = ("dyplom");
-imgk2w1Slide2.src = dyplomyPoziom[23];
+imgk2w1Slide2.src = dyplomyPoziom[11];
 document.querySelector("#k2w1Slide2").appendChild(imgk2w1Slide2);
 
 let imgk2w2Slide2 = document.createElement('img');
 imgk2w2Slide2.alt = ("dyplom");
-imgk2w2Slide2.src = dyplomyPoziom[22];
+imgk2w2Slide2.src = dyplomyPoziom[7];
 document.querySelector("#k2w2Slide2").appendChild(imgk2w2Slide2);
 
 let imgk2w3Slide2 = document.createElement('img');
 imgk2w3Slide2.alt = ("dyplom");
-imgk2w3Slide2.src = dyplomyPoziom[21];
+imgk2w3Slide2.src = dyplomyPoziom[3];
 document.querySelector("#k2w3Slide2").appendChild(imgk2w3Slide2);
 
 let imgk3w1Slide2 = document.createElement('img');
 imgk3w1Slide2.alt = ("dyplom");
-imgk3w1Slide2.src = dyplomyPoziom[20];
+imgk3w1Slide2.src = dyplomyPoziom[10];
 document.querySelector("#k3w1Slide2").appendChild(imgk3w1Slide2);
 
 let imgk3w2Slide2 = document.createElement('img');
 imgk3w2Slide2.alt = ("dyplom");
-imgk3w2Slide2.src = dyplomyPoziom[19];
+imgk3w2Slide2.src = dyplomyPoziom[6];
 document.querySelector("#k3w2Slide2").appendChild(imgk3w2Slide2);
 
 let imgk3w3Slide2 = document.createElement('img');
 imgk3w3Slide2.alt = ("dyplom");
-imgk3w3Slide2.src = dyplomyPoziom[18];
+imgk3w3Slide2.src = dyplomyPoziom[2];
 document.querySelector("#k3w3Slide2").appendChild(imgk3w3Slide2);
 
 let imgk4w1Slide2 = document.createElement('img');
 imgk4w1Slide2.alt = ("dyplom");
-imgk4w1Slide2.src = dyplomyPion[9];
+imgk4w1Slide2.src = dyplomyPion[4];
 document.querySelector("#k4w1Slide2").appendChild(imgk4w1Slide2);
 
 let imgk4w2Slide2 = document.createElement('img');
 imgk4w2Slide2.alt = ("dyplom");
-imgk4w2Slide2.src = dyplomyPion[8];
+imgk4w2Slide2.src = dyplomyPion[1];
 document.querySelector("#k4w2Slide2").appendChild(imgk4w2Slide2);
 
 let imgk5w1Slide2 = document.createElement('img');
 imgk5w1Slide2.alt = ("dyplom");
-imgk5w1Slide2.src = dyplomyPoziom[17];
+imgk5w1Slide2.src = dyplomyPoziom[9];
 document.querySelector("#k5w1Slide2").appendChild(imgk5w1Slide2);
 
 let imgk5w2Slide2 = document.createElement('img');
 imgk5w2Slide2.alt = ("dyplom");
-imgk5w2Slide2.src = dyplomyPoziom[16];
+imgk5w2Slide2.src = dyplomyPoziom[5];
 document.querySelector("#k5w2Slide2").appendChild(imgk5w2Slide2);
 
 let imgk5w3Slide2 = document.createElement('img');
 imgk5w3Slide2.alt = ("dyplom");
-imgk5w3Slide2.src = dyplomyPoziom[15];
+imgk5w3Slide2.src = dyplomyPoziom[1];
 document.querySelector("#k5w3Slide2").appendChild(imgk5w3Slide2);
 
 let imgk6w1Slide2 = document.createElement('img');
 imgk6w1Slide2.alt = ("dyplom");
-imgk6w1Slide2.src = dyplomyPoziom[14];
+imgk6w1Slide2.src = dyplomyPoziom[8];
 document.querySelector("#k6w1Slide2").appendChild(imgk6w1Slide2);
 
 let imgk6w2Slide2 = document.createElement('img');
 imgk6w2Slide2.alt = ("dyplom");
-imgk6w2Slide2.src = dyplomyPoziom[13];
+imgk6w2Slide2.src = dyplomyPoziom[4];
 document.querySelector("#k6w2Slide2").appendChild(imgk6w2Slide2);
 
 let imgk6w3Slide2 = document.createElement('img');
 imgk6w3Slide2.alt = ("dyplom");
-imgk6w3Slide2.src = dyplomyPoziom[12];
+imgk6w3Slide2.src = dyplomyPoziom[0];
 document.querySelector("#k6w3Slide2").appendChild(imgk6w3Slide2);
 
 let imgk7w1Slide2 = document.createElement('img');
 imgk7w1Slide2.alt = ("dyplom");
-imgk7w1Slide2.src = dyplomyPion[7];
+imgk7w1Slide2.src = dyplomyPion[3];
 document.querySelector("#k7w1Slide2").appendChild(imgk7w1Slide2);
 
 let imgk7w2Slide2 = document.createElement('img');
 imgk7w2Slide2.alt = ("dyplom");
-imgk7w2Slide2.src = dyplomyPion[6];
+imgk7w2Slide2.src = dyplomyPion[0];
 document.querySelector("#k7w2Slide2").appendChild(imgk7w2Slide2);
 
 
